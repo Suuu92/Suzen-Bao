aca8ef5b28d559ba701faf95d107d08b0eeeb3fe
Suuu92 <125599121+Suuu92@users.noreply.github.com>
2023-02-19 14:41:22 -0800

    Add files via upload

---
+++ Inheritance/src/Problems.java
@@ -0,0 +1,25 @@
+public class Problems {
+/*
+1.A) Class Z cannot extend both X and Y at the same time.
+B) The subclass Y does not have 'super()' method, so it cannot access the superclass X's constructor.
+C) There's no subclass here, so we cannot use 'super()' method.
+D) The int 'm' is private, so class Y cannot access to it.
+
+2.A) 10
+
+B) 27
+
+C) Class A Constructor
+   Class B Constructor
+   Class C Constructor
+
+D) 51
+   260
+   520
+
+E) 1
+
+F) 200
+
+ */
+}
