fc3a77cf85158fd5e4ebe84944be52c15d5fb902
Suuu92 <125599121+Suuu92@users.noreply.github.com>
2023-02-19 23:18:12 -0800

    Polymorphism

---
+++ Polymorphism/src/Problems.java
@@ -0,0 +1,28 @@
+public class Problems {
+/*
+1. Yes, it is. All 'int' and 'double' parameters are properly used.
+
+2.A) ONE
+
+B) Class Y
+   Class Y
+   Error: 'Y cannot be cast to Z'
+
+C) 1
+   2
+   4
+   3
+
+D) 4
+   2
+   1
+   3
+
+E) Error: 'There's a StackOverFlowError due to infinite recursion,
+           since Class X and Y just run the other's method repeatedly.'
+
+F) 333
+   444
+
+ */
+}
